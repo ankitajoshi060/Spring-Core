da7479a78ed0451b81fe00737a815137a6893b1f
ankitajoshi060 <ankitajoshi060@gamil.com>
2024-01-07 16:03:07 +0530

    added new project

---
+++ Autowiring/src/main/java/com/autowiring/Company.java
@@ -0,0 +1,47 @@
+package com.autowiring;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Company {
+
+	private int companyId;
+	private String companyName;
+	
+	private Employee employee;
+
+	public Company() {
+		super();
+	}
+	
+	public Company(int companyId, String companyName, Employee employee) {
+		super();
+		this.companyId = companyId;
+		this.companyName = companyName;
+		this.employee = employee;
+	}
+	
+	public int getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	@Override
+	public String toString() {
+		return "Company [companyId=" + companyId + ", companyName=" + companyName + ", employee=" + employee + "]";
+	}
+	
+}
