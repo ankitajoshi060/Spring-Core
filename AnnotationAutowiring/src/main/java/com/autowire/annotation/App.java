fda6ac44d1144c188057aac549b2bfadf91e6b65
ankitajoshi060 <ankitajoshi060@gamil.com>
2024-01-08 11:19:24 +0530

    added new project

---
+++ AnnotationAutowiring/src/main/java/com/autowire/annotation/App.java
@@ -0,0 +1,14 @@
+package com.autowire.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+         Company company = context.getBean("company", Company.class);
+         System.out.println(company);
+    }
+}
---
+++ AnnotationAutowiring/src/main/java/com/autowire/annotation/Employee.java
@@ -0,0 +1,36 @@
+package com.autowire.annotation;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(int employeeId, String employeeName) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + "]";
+	}
+	
+}
